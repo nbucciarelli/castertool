0ccc32b232fa1dea762d9dd9e9c966ad4717c8dc
Nick Bucciarelli <nbucciarelli@weddingwire.com>
2019-09-13 19:07:20 -0400

    Removed unnecessary assets.

--- src/components/Champion/List.js
+++
@@ -1,9 +0,0 @@
-// import React, { Component } from 'react';
-
-// class ChampionList extends Component {
-//   render() {
-//     return <h1>Hello, {this.props.name}</h1>;
-//   }
-// }
-
-// export default Champion
